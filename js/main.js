c519446bb9e72d546e1b17ad899d6d1b25964234
MarionFoto <marionacp@gmail.com>
2017-05-15 13:44:56 -0400

    creando los elementos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-Email").value;
+	var password = document.getElementById("input-Password").value;
+	var selecion = document.getElementsByClassName("form-control").selectedIndex;
+
+
 }
